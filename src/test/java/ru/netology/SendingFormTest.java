2d74ed0ae90749abebbd0407af82668e042405db
Yuliya.S <yuliyamarketplace@yandex.ru>
2024-11-01 17:11:40 +0400

    find the error

--- src/test/java/ru/netology/SendingFormTest.java
+++ src/test/java/ru/netology/SendingFormTest.java
@@ -1,3 +1,4 @@
+package ru.netology;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
@@ -15,45 +16,43 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.util.List;
 import org.openqa.selenium.chrome.ChromeOptions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.openqa.selenium.By.cssSelector;
 
 public class SendingFormTest {
     private WebDriver driver;
-}
-
-@BeforeAll
-static void setUpAll() {
-    WebDriverManager.chromedriver().setup();
-}
 
 
-@BeforeEach
-void setUp() {
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--disable-dev-shm-usage");
-    options.addArguments("--no-sandbox");
-    options.addArguments("--headless");
-    ChromeDriver driver = new ChromeDriver(options);
-    driver.get("http://localhost:9999");
-}
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
 
-@AfterEach
-void tearDown() {
-    driver.quit();
-    driver = null;
-}
 
-@Test
-void shouldSendform() {
-    By driver = null;
-    List<WebElement> inputs = driver.findElements(By.cssSelector("input"));
-    inputs.get(0).sendKeys("Юлия");
-    inputs.get(1).sendKeys("+79272230350");
-    driver.findElement((SearchContext) By.cssSelector("[data-test-id='argument']")).click();
-    driver.findElement((SearchContext) By.cssSelector("[data-test-id='submit']")).click();
-    String text = driver.findElement(By.cssSelector("[data-test-id='callbask-success']").getText().trim();
-    assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с Вами в ближайшее время.", text);
-}
-}
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
 
-public void main() {
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldSendform() {
+        By driver = null;
+        List<WebElement> inputs = driver.findElements(cssSelector("input"));
+        inputs.get(0).sendKeys("Юлия");
+        inputs.get(1).sendKeys("+79272230350");
+        driver.findElement((SearchContext) cssSelector("[data-test-id='argument']")).click();
+        driver.findElement((SearchContext) cssSelector("[data-test-id='submit']")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='callbaсk-success']")).getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с Вами в ближайшее время.", text);
+    }
 }
