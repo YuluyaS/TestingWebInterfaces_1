435641f049a796b1005edc165002b694dcadefa1
Yuliya.S <yuliyamarketplace@yandex.ru>
2024-11-11 00:25:13 +0400

    put a badge on the assembly

--- src/test/java/ru/netology/SendingFormTest.java
+++ src/test/java/ru/netology/SendingFormTest.java
@@ -49,13 +49,10 @@ public class SendingFormTest {
     void shouldSendform() {
 
         List<WebElement> inputs = driver.findElements(cssSelector("input"));
-
-        inputs.get(0).sendKeys("Святынина Юлия");
-        inputs.get(1).sendKeys("+79272230350");
-
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Святынина Юлия");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79272230350");
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.className("button")).click();
-
         String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
     }
